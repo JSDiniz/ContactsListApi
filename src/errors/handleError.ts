004b587d081a980cebcb744ef1c20dc635c147d0
Diniz <juniosantos_@hotmail.com>
2023-05-26 15:26:58 -0300

    feat: add errors AppError and handleError

---
+++ src/errors/handleError.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./AppError";
+
+const handleError = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: "Internal Server Error." });
+};
+export default handleError;

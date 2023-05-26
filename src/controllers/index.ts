c7384193f13691bc5869bcdda28b7501ff0eb254
Diniz <juniosantos_@hotmail.com>
2023-05-26 18:28:11 -0300

    feat: add list users controller index

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
-
+// Users
 import createUserController from "./users/createUser.controller";
+import listUsersController from "./users/listUsers.controller";
 
-export default createUserController
+export {createUserController, listUsersController}

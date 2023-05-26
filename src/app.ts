e5d25af26ce2efc6e23a51f2467f2b8f88a8a4ef
Diniz <juniosantos_@hotmail.com>
2023-05-26 15:26:26 -0300

    feat: add app handleError

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import "express-async-errors"
 import cors from "cors"
+import handleError from "./errors/handleError"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+
+app.use(handleError)
 
 
 export default app

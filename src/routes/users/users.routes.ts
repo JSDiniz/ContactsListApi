5b0ad4fbcee44bd78a30b56edf754b4b083afc3c
Diniz <juniosantos_@hotmail.com>
2023-05-26 16:50:56 -0300

    refactor: users routers

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,8 +1,10 @@
 import {Router} from "express"
 import createUserController from "../../controllers";
+import { userExistsMiddleware, validDataMiddleware } from "../../middlewares";
+import { userSchemasReq } from "../../schemas";
 
 const userRouter = Router()
 
-userRouter.post("", createUserController);
+userRouter.post("", validDataMiddleware(userSchemasReq), createUserController);
 
 export default userRouter

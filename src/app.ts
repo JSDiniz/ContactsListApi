49aae5f8cb8b5ef72a2c1725847d0a368a78323e
Diniz <juniosantos_@hotmail.com>
2023-05-26 19:17:38 -0300

    feat: add session Router on app

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,15 @@ import express from "express"
 import "express-async-errors"
 import cors from "cors"
 import handleError from "./errors/handleError"
-import userRouter from "./routes/users/users.routes"
+import { sessionRouter, userRouter } from "./routes"
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use("/users", userRouter)
-
+app.use("/session", sessionRouter)
 
 app.use(handleError)
 

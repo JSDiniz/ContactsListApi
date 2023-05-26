f50031ff5570a8350f5cae5e751dd352cc603312
Diniz <juniosantos_@hotmail.com>
2023-05-26 19:16:55 -0300

    feat: add Middlewares users routes

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,12 +1,12 @@
 import {Router} from "express"
 import {createUserController, listUsersController } from "../../controllers";
-import { userExistsMiddleware, validDataMiddleware } from "../../middlewares";
+import { adminAuthMiddleware, userExistsMiddleware, validDataMiddleware, validTokenMiddleware } from "../../middlewares";
 import { userSchemasReq } from "../../schemas";
 
 const userRouter = Router()
 
 userRouter.post("", validDataMiddleware(userSchemasReq), createUserController);
 
-userRouter.get("", listUsersController);
+userRouter.get("",validTokenMiddleware, adminAuthMiddleware, listUsersController);
 
 export default userRouter

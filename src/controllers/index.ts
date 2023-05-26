18db0a26a359556a7ba2cd0e1133624b85e53ddb
Diniz <juniosantos_@hotmail.com>
2023-05-26 20:21:09 -0300

    feat: add user Id Controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,14 @@
-// Users
-import createUserController from "./users/createUser.controller";
-import listUsersController from "./users/listUsers.controller";
-
 // Session
 import sessionController from "./session/session.controller";
 
-export {createUserController, listUsersController,sessionController}
+// Users
+import createUserController from "./users/createUser.controller";
+import listUsersController from "./users/listUsers.controller";
+import userIdController from "./users/userId.controller";
+
+export {
+  sessionController,
+  createUserController,
+  listUsersController,
+  userIdController,
+};

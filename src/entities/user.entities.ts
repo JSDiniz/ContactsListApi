b1577a707b382c3ce5474e2c2eb3c70d8ee33161
Diniz <juniosantos_@hotmail.com>
2023-05-26 15:52:48 -0300

    feat: add user entities

---
+++ src/entities/user.entities.ts
@@ -0,0 +1,66 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeUpdate,
+    BeforeInsert,
+
+  } from "typeorm";
+  import { getRounds, hashSync } from "bcryptjs";
+  
+  @Entity("users")
+  class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+  
+    @Column({ type: "varchar", length: 60 })
+    name: string;
+  
+    @Column({ type: "varchar", length: 100, unique: true })
+    email: string;
+  
+    @Column({ type: "varchar", length: 120 })
+    password: string;
+  
+    @Column({ type: "varchar", length: 20 })
+    phone: string;
+    
+    @Column({ default: false })
+    isAdmin: boolean;
+  
+    @Column({ default: true })
+    isActive: boolean;
+
+    @Column({ nullable: true })
+    imageUrl: string;
+  
+    @CreateDateColumn({ type: "date" })
+    createdAt: Date;
+  
+    @UpdateDateColumn({ type: "date" })
+    updatedAt: Date;
+  
+    @BeforeUpdate()
+    @BeforeInsert()
+    hashPassword() {
+      const isEncrypted = getRounds(this.password);
+      if (!isEncrypted) {
+        this.password = hashSync(this.password, 10);
+      }
+    }
+  
+    @BeforeInsert()
+    transformNameUpperEmailLowerCase() {
+      this.email = this.email.toLocaleLowerCase();
+      this.name = this.name
+        .toLocaleLowerCase()
+        .replace(/(^\w{1})|(\s+\w{1})/g, (letra: string) => letra.toUpperCase());
+    }
+  
+
+  }
+  
+  export { User };
+  

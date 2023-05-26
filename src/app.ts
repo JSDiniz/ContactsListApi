e02d767ebc7e9215f6106b1e6a8238ad25ebbbb2
Diniz <juniosantos_@hotmail.com>
2023-05-26 15:56:03 -0300

    feat: add user Router on app

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from "express"
 import "express-async-errors"
 import cors from "cors"
 import handleError from "./errors/handleError"
+import userRouter from "./routes/users/users.routes"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use("/users", userRouter)
 
 
 app.use(handleError)

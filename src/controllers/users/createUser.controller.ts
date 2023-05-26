c067d9079c4b15a6c32b95a2f37a77c7b390c139
Diniz <juniosantos_@hotmail.com>
2023-05-26 18:27:34 -0300

    refactor: create User Controller

--- src/controllers/users/createUser.controller.ts
+++ src/controllers/users/createUser.controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express"
-import createUserService from "../../services";
+import { createUserService } from "../../services";
 
 const createUserController = async (req: Request, res: Response) => {
 

024814bf81c1ffbefb2591d38257b0f6f3977be5
Diniz <juniosantos_@hotmail.com>
2023-05-26 19:13:07 -0300

    feat: add valid Token Middleware index

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,6 @@
-import userExistsMiddleware from "./userExist.middleware";
 import validDataMiddleware from "./validData.middleware";
+import userExistsMiddleware from "./userExist.middleware";
+import validTokenMiddleware from "./validToken.middleware";
+import adminAuthMiddleware from "./adminAuth.middleware";
 
-export {validDataMiddleware, userExistsMiddleware}
+export {validDataMiddleware, userExistsMiddleware, validTokenMiddleware, adminAuthMiddleware}

81bc85f84680ba610ac15c6b7fa870059a1a11f9
Diniz <juniosantos_@hotmail.com>
2023-05-26 18:30:23 -0300

    feat: add list Users Controller users routes

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,10 +1,12 @@
 import {Router} from "express"
-import createUserController from "../../controllers";
+import {createUserController, listUsersController } from "../../controllers";
 import { userExistsMiddleware, validDataMiddleware } from "../../middlewares";
 import { userSchemasReq } from "../../schemas";
 
 const userRouter = Router()
 
 userRouter.post("", validDataMiddleware(userSchemasReq), createUserController);
+
+userRouter.get("", listUsersController);
 
 export default userRouter

e5721781fe654b6ac91a3d1a0de4327e5538dc2b
Diniz <juniosantos_@hotmail.com>
2023-05-26 16:49:30 -0300

    feat: modifying create User Service

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -1,6 +1,29 @@
-import IUserReq from "../../interfaces"
+import AppDataSource from "../../data-source"
+import { User } from "../../entities/user.entities"
+import { AppError } from "../../errors/AppError"
+import { IUserReq, IUserRes } from "../../interfaces"
+import { userSchemasRes } from "../../schemas"
 
 
-const createUserService  = (body: IUserReq) => {}
+const createUserService  = async (body: IUserReq): Promise<IUserRes> => {
+
+    const usergetRepository = AppDataSource.getRepository(User)
+    const userExist = await usergetRepository.findOneBy({ email: body.email });
+  
+    if (userExist) {
+      throw new AppError("User already exists!", 409);
+    }
+
+    const user =  usergetRepository.create(body)
+
+    await usergetRepository.save(user)
+
+    const validUser = await userSchemasRes.validate(user, {
+        stripUnknown: true,
+      });
+
+    return validUser
+
+}
 
 export default createUserService

8cb3a028519f77c3c3385137e0f32ce2a30013fd
Diniz <juniosantos_@hotmail.com>
2023-05-26 19:14:11 -0300

    feat: add session Controller index

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,7 @@
 import createUserController from "./users/createUser.controller";
 import listUsersController from "./users/listUsers.controller";
 
-export {createUserController, listUsersController}
+// Session
+import sessionController from "./session/session.controller";
+
+export {createUserController, listUsersController,sessionController}

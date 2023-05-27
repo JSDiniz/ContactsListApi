3350b41955db38a856aed66e105144101685b38e
Diniz <juniosantos_@hotmail.com>
2023-05-26 21:31:22 -0300

    feat: add delete User Service

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,14 @@ import sessionService from "./session/session.service";
 
 // Users
 import createUserService from "./users/createUser.service";
+import deleteUserService from "./users/deleteUser.service";
 import listUsersService from "./users/listUsers.service";
 import userIdService from "./users/userId.service";
 
-export { sessionService, createUserService, listUsersService, userIdService };
+export {
+  sessionService,
+  createUserService,
+  deleteUserService,
+  listUsersService,
+  userIdService,
+};

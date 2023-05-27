3ba11a05470b427ebd77a59f6cd36b01a934760f
Diniz <juniosantos_@hotmail.com>
2023-05-27 12:19:30 -0300

    feat: add contact Router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
+import contactRouter from "./contacts/contacts.routes";
 import sessionRouter from "./session/session.routes";
 import userRouter from "./users/users.routes";
 
-export {userRouter, sessionRouter}
+export { userRouter, sessionRouter, contactRouter };

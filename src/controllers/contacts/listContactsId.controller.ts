9b021dc7ae58aecc35824874ad1487d9206b4e1f
Diniz <juniosantos_@hotmail.com>
2023-05-27 15:14:12 -0300

    refactor: name of list Contacts User Controller

--- src/controllers/contacts/listContactsId.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { listContactsIdService } from "../../services";
-
-const listContactsIdController = async (req: Request, res: Response) => {
-  const data = await listContactsIdService(req.params.id);
-  return res.status(200).json(data);
-};
-
-export default listContactsIdController;

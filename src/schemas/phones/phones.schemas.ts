73bb0d44d3c5978c511c12029bf794a2d8271eb5
Diniz <juniosantos_@hotmail.com>
2023-05-27 12:23:22 -0300

    feat: add phones schemas

---
+++ src/schemas/phones/phones.schemas.ts
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+import { SchemaOf } from "yup";
+import { IPhonesReq, IPhonesRes } from "../../interfaces";
+
+const phonesSchemaReq: SchemaOf<IPhonesReq> = Yup.object().shape({
+  phone: Yup.string()
+    .max(20)
+    .required("Telefone Obrigatorio")
+    .matches(
+      /^[0-9]([0-9]{8}|[0-9]{9})/,
+      "Deve ser um número de telefone válido"
+    ),
+});
+
+const phonesSchemaRes: SchemaOf<IPhonesRes> = Yup.object().shape({
+  updatedAt: Yup.date().notRequired(),
+  createdAt: Yup.date().notRequired(),
+  phone: Yup.string().notRequired(),
+  id: Yup.string().notRequired(),
+});
+
+export { phonesSchemaReq, phonesSchemaRes };

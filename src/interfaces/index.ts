f6f5f0efa14e2186eb819416edb1f756279b95fe
Diniz <juniosantos_@hotmail.com>
2023-05-27 12:22:38 -0300

    feat: add contacts, phones and email on interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,28 @@
 // Users
 import { IUserReq, IUserRes, IUserUpdate } from "./users/users.interfaces";
 
-//session
+// Session
 import { ISessionReq, ISessionRes } from "./session/session.interfaces";
 
-export { IUserReq, IUserRes, IUserUpdate, ISessionReq, ISessionRes };
+// Contacts
+import { IContactsReq, IContactsRes } from "./contacts/contacts.interfaces";
+
+// Phones
+import { IPhonesReq, IPhonesRes } from "./phones/phones.interfaces";
+
+// Emails
+import { IEmailsReq, IEmailsRes } from "./emails/emails.interfaces";
+
+export {
+  IUserReq,
+  IUserRes,
+  IUserUpdate,
+  ISessionReq,
+  ISessionRes,
+  IContactsReq,
+  IContactsRes,
+  IPhonesReq,
+  IPhonesRes,
+  IEmailsReq,
+  IEmailsRes,
+};

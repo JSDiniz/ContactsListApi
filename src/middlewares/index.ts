1c62b45ff6480a5e8c80267a9b062244fea9968e
Diniz <juniosantos_@hotmail.com>
2023-05-26 21:34:58 -0300

    feat: add User Or Admin and IsActive on Middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,14 @@ import validDataMiddleware from "./validData.middleware";
 import userExistsMiddleware from "./userExist.middleware";
 import validTokenMiddleware from "./validToken.middleware";
 import adminAuthMiddleware from "./adminAuth.middleware";
+import userIsActiveMiddleware from "./userIsActive.middleware";
+import verifyUserOrAdminMiddleware from "./verifyUserOrAdmin.middleware";
 
-export {validDataMiddleware, userExistsMiddleware, validTokenMiddleware, adminAuthMiddleware}
+export {
+  validDataMiddleware,
+  userExistsMiddleware,
+  validTokenMiddleware,
+  adminAuthMiddleware,
+  userIsActiveMiddleware,
+  verifyUserOrAdminMiddleware,
+};

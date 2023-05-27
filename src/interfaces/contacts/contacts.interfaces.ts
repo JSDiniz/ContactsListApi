8e46094774e854e9560f0bc52dd8ec0295be29d6
Diniz <juniosantos_@hotmail.com>
2023-05-27 12:21:35 -0300

    feat: add contacts interfaces

---
+++ src/interfaces/contacts/contacts.interfaces.ts
@@ -0,0 +1,21 @@
+import { IEmailsReq, IEmailsRes } from "../emails/emails.interfaces";
+import { IPhonesReq, IPhonesRes } from "../phones/phones.interfaces";
+import { IUserRes } from "../users/users.interfaces";
+
+export interface IContactsReq {
+  name: string;
+  phones: Array<IPhonesReq>;
+  emails: Array<IEmailsReq>;
+  imageUrl: string;
+}
+
+export interface IContactsRes {
+  id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  users: IUserRes;
+  phones: Array<IPhonesRes>;
+  emails: Array<IEmailsRes>;
+  imageUrl: string;
+}

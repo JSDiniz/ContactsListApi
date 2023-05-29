7ef5f40544273099f85d90b49e6706024fb63dfa
Diniz <juniosantos_@hotmail.com>
2023-05-29 15:32:38 -0300

    feat: add email Not Exist Middleware

---
+++ src/middlewares/emailNotExist.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import AppDataSource from "../data-source";
+import { Contacts } from "../entities/contacts.entity";
+import { AppError } from "../errors/AppError";
+import { Emails } from "../entities/emails.entity";
+
+const emailNotExistMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const emailRepository = AppDataSource.getRepository(Emails);
+
+  const email = await emailRepository.findOneBy({ id: req.params.id });
+
+  if (!email) {
+    throw new AppError("Email does not exist!", 409);
+  }
+
+  return next();
+};
+
+export default emailNotExistMiddleware;

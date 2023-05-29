c76f44ba0b6dbf2f33a2e3e3ba78403df4c84bb4
Diniz <juniosantos_@hotmail.com>
2023-05-29 10:21:50 -0300

    feat: add updateEmailsSchemaReq on schemas

--- src/schemas/emails/emails.schemas.ts
+++ src/schemas/emails/emails.schemas.ts
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 import { SchemaOf } from "yup";
-import { IEmailsReq, IEmailsRes } from "../../interfaces";
+import { IEmailsReq, IEmailsRes, IUpdateEmailsRes } from "../../interfaces";
 
 const emailsSchemaReq: SchemaOf<IEmailsReq> = Yup.object().shape({
   email: Yup.string()
@@ -16,4 +16,9 @@ const emailsSchemaRes: SchemaOf<IEmailsRes> = Yup.object().shape({
   id: Yup.string().notRequired().notRequired(),
 });
 
-export { emailsSchemaReq, emailsSchemaRes };
+const updateEmailsSchemaReq: SchemaOf<IUpdateEmailsRes> = Yup.object().shape({
+  email: Yup.string().notRequired(),
+  id: Yup.string().notRequired().notRequired(),
+});
+
+export { emailsSchemaReq, emailsSchemaRes, updateEmailsSchemaReq };

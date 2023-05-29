887452bf5144ab54220ef414f974fd3af5c240e5
Diniz <juniosantos_@hotmail.com>
2023-05-29 16:38:11 -0300

    feat: add emailRouter and phoneRouter on Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import contactRouter from "./contacts/contacts.routes";
+import emailRouter from "./emails/emails.routes";
+import phoneRouter from "./phones/phones.routes";
 import sessionRouter from "./session/session.routes";
 import userRouter from "./users/users.routes";
 
-export { userRouter, sessionRouter, contactRouter };
+export { userRouter, sessionRouter, contactRouter, phoneRouter, emailRouter };

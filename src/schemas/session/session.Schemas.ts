9ee7be8c8cf5a656d67f6e3038ae96c83df0ad60
Diniz <juniosantos_@hotmail.com>
2023-05-30 18:04:55 -0300

    refactor: sessionSchemaRes

--- src/schemas/session/session.Schemas.ts
+++ src/schemas/session/session.Schemas.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 import { SchemaOf } from "yup";
 import { ISessionReq, IUser } from "../../interfaces";
-import { updatecontactSchemasReq } from "../contacts/contacts.schemas";
+import { contactSchemas } from "../contacts/contacts.schemas";
 
 const sessionSchemaReq: SchemaOf<ISessionReq> = yup.object().shape({
   email: yup.string().email("Must be a valid email").max(60).required(),
@@ -9,7 +9,7 @@ const sessionSchemaReq: SchemaOf<ISessionReq> = yup.object().shape({
 });
 
 const sessionSchemaRes: SchemaOf<IUser> = yup.object().shape({
-  contacts: yup.array(updatecontactSchemasReq).notRequired(),
+  contacts: yup.array(contactSchemas).notRequired(),
   name: yup.string().notRequired(),
   id: yup.string().notRequired(),
 });
